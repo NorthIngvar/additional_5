b8466a85c5507ca2148cacfc0dc4309c5b254f9d
Killjoy4 <dedovich1.17@gmail.com>
2018-09-29 19:01:37 +0300

    Final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var temporary = str.split('');
+
+  for (let a = 1; a < temporary.length; a++) {
+    for (let b = 0; b < bracketsConfig.length; b++) {
+      if ((temporary[a] === bracketsConfig[b][1]) && (temporary[a - 1] === bracketsConfig[b][0])) {
+        temporary.splice(a - 1, 2);
+        a = a - 2;
+      }
+    }
+  }
+  return (temporary.length > 0) ? false : true;
 }
